a20dd6e1d1fef585255384ea6dc433107c29fae3
Sanjay Rai <sanjayrai@Sanjays-MacBook-Pro.local>
2014-10-07 20:42:38 +0100

    Initial Commit

---
+++ Post Card/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Post Card
+//
+//  Created by Sanjay Rai on 07/10/2014.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
