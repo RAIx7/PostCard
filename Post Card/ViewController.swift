771f767db4d15178ecca4bf5fd6ec697ef2c4bd0
Sanjay Rai <sanjayrai@Sanjays-MacBook-Pro.local>
2014-10-11 14:13:47 +0100

    third commit

--- Post Card/ViewController.swift
+++ Post Card/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
     @IBOutlet weak var mailButton: UIButton!
+    @IBOutlet weak var nameLabel: UILabel!
     
     
     
@@ -35,13 +36,15 @@ class ViewController: UIViewController {
         messageLabel.hidden = false
         messageLabel.text = enterMessageTextField.text
         messageLabel.textColor = UIColor.magentaColor()
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
         
         mailButton.setTitle("SENT MAIL!!!", forState: UIControlState.Normal)
         
         enterMessageTextField.text = ""
-        
-        //Comment to test git push
-
+        enterNameTextField.text = ""
+    
 }
 
 }
